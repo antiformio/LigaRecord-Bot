c4b4f496e0a12c907e0a2fbfbfc282601899e985
antiformio <fjnmgm@gmail.com>
2019-09-14 22:49:22 +0200

    Initial commit

---
+++ botRecord.py
@@ -0,0 +1,441 @@
+import os
+import pickle
+import smtplib
+import time
+from email.mime.application import MIMEApplication
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import json
+import numpy as np
+import pandas as pd
+import requests
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+import serialize
+
+
+def readCredentials():
+    with open("C:\\Users\\fhm\\Desktop\\botRecordVsCode\\credentials.json") as f:
+        data = json.load(f)
+    return data["email"], data["password"], data["gecko_path"]
+
+
+def getData():
+    """
+        Web-Scrapping site da liga record.
+    """
+    options = Options()
+    options.headless = False
+
+    email, pass_word, gecko_path = readCredentials()
+
+    browser = webdriver.Firefox(options=options, executable_path=gecko_path)
+    browser.get(
+        "https://aminhaconta.xl.pt/LoginNonio?returnUrl=https%3a%2f%2fliga.record.pt%2fdefault.aspx"
+    )
+
+    user = browser.find_element_by_css_selector("#email")
+    user.send_keys(email)
+    time.sleep(5)
+
+    password = browser.find_element_by_xpath(
+        "/html/body/section/div/div/div/div[2]/form/div[2]"
+    )
+    password.click()
+    realpass = browser.find_element_by_css_selector("#password")
+    realpass.send_keys(pass_word)
+
+    browser.find_element_by_css_selector("#loginBtn").click()
+    time.sleep(30)
+
+    browser.find_element_by_xpath("/html/body/div[1]/div/div/div[2]/button[2]").click()
+
+    browser.find_element_by_css_selector("span.i-shield-lock").click()  # Ligas Privadas
+    time.sleep(5)
+    browser.find_element_by_css_selector(".ranking").click()
+    time.sleep(5)
+
+    browser.find_element_by_xpath("/html/body/footer/div[2]/i").click()
+    time.sleep(5)
+
+    dropdown = browser.find_element_by_xpath(
+        "/html/body/form/section[2]/div[2]/div[2]/div[2]/div/div[2]/div[1]/div/button"
+    )
+    dropdown.click()
+    time.sleep(5)
+
+    browser.find_element_by_xpath(
+        "/html/body/form/section[2]/div[2]/div[2]/div[2]/div/div[2]/div[1]/div/ul/li[2]/a"
+    ).click()
+    time.sleep(3)
+
+    equipas = browser.find_element_by_xpath(
+        '//*[@id="main_main_main_ctl00_ltTeamsRound"]'
+    )
+    todas = equipas.find_elements_by_tag_name("h5")
+
+    rondaObject = browser.find_element_by_xpath('//*[@id="topContainer_ctl00_round"]')
+    numeroRondaObject = rondaObject.find_elements_by_tag_name("h2")
+    jornada = numeroRondaObject[0].text
+
+    list = []
+    for equipaNome in todas:
+        list.append(equipaNome.text)
+    browser.quit()
+
+    resultadosDict = {list[i]: list[i + 1] for i in range(0, len(list), 2)}
+
+    return resultadosDict, jornada
+
+
+def getTable():
+    """
+        Download da tabela classificativa
+    """
+    readSave = serialize.serialization()
+    readSave.AWSdownload("Tabela", "TableDownloaded")
+    tabelaDownloaded = pickle.load(open("TableDownloaded", "rb"))
+    os.remove("TableDownloaded")
+    return tabelaDownloaded
+
+
+def getCalendar(jornada):
+    """
+        Download do ficheiro do calendário, de uma determinada jornada
+    """
+    readSave = serialize.serialization()
+    readSave.AWSdownload(f"Calendario{jornada}", f"Calendario{jornada}")
+    calendario = pickle.load(open(f"Calendario{jornada}", "rb"))
+    os.remove(f"Calendario{jornada}")
+    return calendario
+
+
+def telegram_bot_sendtext(bot_message):
+    """
+        Recebe mensagem por parametro e envia para o bot.
+    """
+    bot_token = "750941750:AAHOkacoPsz6Zknk5P2_f9_1eSReu8yy7sA"
+    bot_chatID = "792080037"
+    send_text = (
+        "https://api.telegram.org/bot"
+        + bot_token
+        + "/sendMessage?chat_id="
+        + bot_chatID
+        + "&parse_mode=Markdown&text="
+        + bot_message
+    )
+    requests.get(send_text)
+
+
+def __getResultByJogo(casa, fora, dict):
+    """
+        Função privada para calcular a mensagem dos resultados para enviar por telegram.
+    """
+    resultCasa = dict[casa]
+    resultFora = dict[fora]
+    return (
+        "\n"
+        + str(casa)
+        + ": "
+        + resultCasa
+        + " "
+        + str(fora)
+        + ": "
+        + resultFora
+        + "\n"
+    )
+
+
+def buildResult(tuplos, dictPontuacoes):
+    """
+        Produz tanto a string com os resultados (para enviar por telegram) como a lista com um dicionário por jogo.
+        A listaResultados tem o formato : [{'equipa1': 20, 'equipa2': 30}, {'equipa3': 12, 'equipa4': 38}]
+    """
+    resultadoString = ""
+    listaResultados = []
+    for jogo in tuplos:
+        casa, fora = jogo[0], jogo[1]
+        resultadoString += __getResultByJogo(casa, fora, dictPontuacoes)
+        newDic = {
+            jogo[0]: int(dictPontuacoes[jogo[0]]),
+            jogo[1]: int(dictPontuacoes[jogo[1]]),
+        }
+        listaResultados.append(newDic)
+    return resultadoString, listaResultados
+
+
+def updateTabela(listaResult, tabela):
+    """
+        Actualiza tabela. Em função do resultado de cada jogo da jornada.
+    """
+    for dic in listaResult:
+        maiorPontuacao = 0
+        equipaVencedora = ""
+        pontuacoes = list(dic.values())
+        if pontuacoes[0] == pontuacoes[1]:
+            equipaVencedora = "empate"
+            for equipa, pontos in dic.items():
+                row = tabela.loc[tabela["Equipa"] == equipa].index.values.astype(int)[0]
+                tabela.at[row, "Pontos"] += 1
+                tabela.at[row, "Jogos"] += 1
+                tabela.at[row, "E"] += 1
+                tabela.at[row, "GM"] += pontos
+                tabela.at[row, "GS"] += pontos
+        else:
+            for equipa, pontos in dic.items():
+                if pontos > maiorPontuacao:
+                    maiorPontuacao = pontos
+                    equipaVencedora = equipa
+            for equipa, pontos in dic.items():
+                if equipa != equipaVencedora:
+                    equipaPerdedora = equipa
+                    pontuacaoPerdedora = pontos
+            # Equipa Vencedora
+            row = tabela.loc[tabela["Equipa"] == equipaVencedora].index.values.astype(
+                int
+            )[0]
+            tabela.at[row, "Pontos"] += 3
+            tabela.at[row, "Jogos"] += 1
+            tabela.at[row, "V"] += 1
+            tabela.at[row, "GM"] += maiorPontuacao
+            tabela.at[row, "GS"] += pontuacaoPerdedora
+            tabela.at[row, "GA"] = tabela.at[row, "GM"] - tabela.at[row, "GS"]
+            # Equipa Perdedora
+            row = tabela.loc[tabela["Equipa"] == equipaPerdedora].index.values.astype(
+                int
+            )[0]
+            tabela.at[row, "Jogos"] += 1
+            tabela.at[row, "D"] += 1
+            tabela.at[row, "GM"] += pontuacaoPerdedora
+            tabela.at[row, "GS"] += maiorPontuacao
+            tabela.at[row, "GA"] = tabela.at[row, "GM"] - tabela.at[row, "GS"]
+    return tabela
+
+
+def saveResultsDict(dictPontuacoes, jornada):
+    """
+        Upload para a s3 dos dicionários com os resultados por jornada
+    """
+    readSave = serialize.serialization()
+    pickle.dump(dictPontuacoes, open(jornada, "wb"))
+    readSave.AWSupload(jornada, jornada)
+    os.remove(jornada)
+
+
+def saveUpdatedTable(tabelaUpdated):
+    """
+        Upload para a s3 da tabela actualizada (depois de feitas todas as alterações, de classificacao e melhores equipas)
+    """
+    readSave = serialize.serialization()
+    pickle.dump(tabelaUpdated, open("Tabela", "wb"))
+    readSave.AWSupload("Tabela", "Tabela")
+
+
+def biggestScorer(dictPontuacoes, tabela):
+    """
+        Procura o(s) melhores resultados da jornada e dá mais um ponto a essa(s) equipa(s)
+    """
+    for key in dictPontuacoes:
+        dictPontuacoes[key] = int(dictPontuacoes[key])
+    max_value = max(dictPontuacoes.values())
+    max_keys = [k for k, v in dictPontuacoes.items() if v == max_value]
+
+    for equipa in max_keys:
+        row = tabela.loc[tabela["Equipa"] == equipa].index.values.astype(int)[0]
+        tabela.at[row, "Pontos"] += 1
+    return tabela, max_keys
+
+
+def reduceTableDetails(tabela):
+    """
+        Reduz os detalhes da tabela final. Limita-se a imprimir tabela com ranking equipa e pontos.
+    """
+    tabelaCompact = tabela[["Equipa", "Pontos"]]
+    tabelaSorted = tabelaCompact.sort_values(["Pontos"], ascending=False)
+    tabelaSorted = tabelaSorted.reset_index(drop=True)
+    tabelaSorted.index = np.arange(1, len(tabelaSorted) + 1)
+    return tabelaSorted
+
+
+def tableToHtmlAndEmail(table, bestTeams, jornada):
+    """
+        Ordena a tabela por Pontos descendente. Cria novo index.
+        Converte a tabela para HTML, e envia email.
+    """
+    listaEmails = ["fjnmgm@gmail.com"]  # ,'Teixeira.capela@gmail.com'
+    tableHtml = table.sort_values(["Pontos"], ascending=False).reset_index(drop=True)
+    tableHtml.index = np.arange(1, len(tableHtml) + 1)
+    htmlTable = tableHtml.to_html()
+    textoEmail = (
+        "<b>Resultados da Jornada: </b><br>"
+        + jornada.replace("\n", "<br>")
+        + "<br><br><b>Equipa(s) com melhor pontuaçao (mais um ponto): </b> <br> "
+        + ", ".join(bestTeams)
+        + "<br><br><b>Tabela classificativa: </b>"
+        + htmlTable
+    )
+    sendEMail(textoEmail, jornada, listaEmails)
+
+
+def resetTable():
+    """
+        Function to reset the table to default values. Use carefully because it deletes the previous table.
+    """
+    readSave = serialize.serialization()
+    dataForFrame = {
+        "Equipa": [
+            "Alphateam",
+            "FC Kombichos",
+            "Here For Beer",
+            "FC Chupitos",
+            "ClassOnGrass",
+            "Virose",
+            "FCBalasar",
+            "SL Bernardes",
+            "TascoFC",
+            "Athletic Dafundo",
+            "Chuecos FC",
+            "FC Poukitxo",
+            "Atlético Alijoense",
+            "Black Mamba FC",
+            "Fonte do Olmo FC",
+            "Messishow",
+        ],
+        "Pontos": [0, 3, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 1, 0],
+        "Jogos": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "V": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "E": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "D": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "GM": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "GS": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "GA": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    }
+    tabela = pd.DataFrame(
+        dataForFrame,
+        columns=["Equipa", "Pontos", "Jogos", "V", "E", "D", "GM", "GS", "GA"],
+    )
+    pickle.dump(tabela, open("Tabela", "wb"))
+    readSave.AWSupload("Tabela", "Tabela")
+
+
+def createCalendar():
+    """
+        Function to create calendar. Eatch file (jornada) consists on a list of tuples. filenames is as following: CalendarioX (X being Ronda)
+    """
+    readSave = serialize.serialization()
+    calendario = [
+        ("Athletic Dafundo", "Chuecos FC"),
+        ("FCBalasar", "FC Chupitos"),
+        ("FC Kombichos", "TascoFC"),
+        ("Messishow", "Fonte do Olmo FC"),
+        ("FC Poukitxo", "Here For Beer"),
+        ("Black Mamba FC", "Atlético Alijoense"),
+        ("SL Bernardes", "Alphateam"),
+        ("Virose", "ClassOnGrass"),
+    ]
+    pickle.dump(calendario, open("Calendario2", "wb"))
+    readSave.AWSupload("Calendario2", "Calendario2")
+
+
+def sendEMail(texto, jornada, listaEmails):
+    """
+        Sends the email to the selected mailing list
+    """
+    destino = listaEmails
+    mail = MIMEMultipart()
+    mail["From"] = "aws.py.servidor@gmail.com"
+    mail["To"] = ", ".join(destino)
+    mail["Cc"] = ""
+    mail["Subject"] = f"Misters do Tasco - Resultados da {jornada}"
+    text = ""
+    html = texto
+
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+    mail.attach(part1)
+    mail.attach(part2)
+
+    conn = smtplib.SMTP("smtp.gmail.com", 587)
+    conn.ehlo()
+    conn.starttls()
+    conn.login("aws.py.servidor@gmail.com", "ketooketyr")
+    conn.sendmail(mail["From"], destino, mail.as_string())
+    conn.quit()
+
+
+if __name__ == "__main__":
+    """
+        Vai buscar o dicionário das pontuacoes e a respectiva jornada à liga record
+    """
+    dictPontuacoes, jornada = getData()
+    jornada = "Ronda " + str(int((jornada[-2:].strip())) - 1)
+    tabelaOnServer = getTable()
+
+    ######################################
+    # TESTING ! RETIRAR DO FLUXO DEPOIS
+    ######################################
+    # dictPontuacoes = {
+    #     "Athletic Dafundo": "34",
+    #     "Chuecos FC": "21",
+    #     "FCBalasar": "23",
+    #     "FC Chupitos": "54",
+    #     "FC Kombichos": "87",
+    #     "TascoFC": "23",
+    #     "Messishow": "23",
+    #     "Fonte do Olmo FC": "78",
+    #     "FC Poukitxo": "100",
+    #     "Here For Beer": "66",
+    #     "Black Mamba FC": "99",
+    #     "Atlético Alijoense": "1",
+    #     "SL Bernardes": "55",
+    #     "Alphateam": "33",
+    #     "Virose": "100",
+    #     "ClassOnGrass": "12",
+    # }
+    # jornada = "RONDA 1"
+    ######################################
+    ######################################
+
+    """
+        Salvar o dicionário dos resultados na S3 (formato RONDA X)
+    """
+    # saveResultsDict(dictPontuacoes, jornada)
+
+    """
+        Vai buscar o calendário da jornada respectiva. Calcula a string dos resultados, e a lista com os dados para actualizar a tabela.
+        Envia o telegram com a string dos resultados
+    """
+    calendario = getCalendar(int(jornada[-2:]))
+    resultadosString, listaResultados = buildResult(calendario, dictPontuacoes)
+    telegram_bot_sendtext(resultadosString)
+
+    """
+        Actualiza a tabela com os resultados da jornada.
+        Actualiza novamente com o ponto adicional da(s) melhor(es) equipa(s) da jornada.
+        Salva a nova tabela na S3.
+    """
+    tabelaUpdated = updateTabela(listaResultados, tabelaOnServer)
+    tabelaUpdatedWithBiggestScorer, bestTeams = biggestScorer(
+        dictPontuacoes, tabelaUpdated
+    )
+
+    # saveUpdatedTable(tabelaUpdatedWithBiggestScorer)
+    print(tabelaUpdatedWithBiggestScorer)
+    # print(bestTeams)
+
+    """
+        Edita a tabela para compactar os dados e envia a nova tabela por telegram e email.
+    """
+    tabelaFinal = reduceTableDetails(tabelaUpdatedWithBiggestScorer)
+    telegram_bot_sendtext(tabelaFinal.to_string(header=False))
+    telegram_bot_sendtext("Equipa(s) com melhor pontuaçao: \n" + ", ".join(bestTeams))
+    tableToHtmlAndEmail(tabelaUpdatedWithBiggestScorer, bestTeams, jornada)
+
+    """
+        Manter sempre comentado. Serve para fazer reset à tabela, e criar os calendários (não estão completos...)
+    """
+    # resetTable()
+    # createCalendar()
---
+++ serialize.py
@@ -0,0 +1,71 @@
+import json
+import os
+import pickle
+
+import boto3
+from botocore.exceptions import NoCredentialsError, ClientError
+
+
+class serialization:
+
+    """
+        Loads credentials from JSON and reads into class variables
+    """
+
+    def readCredentials(self):
+        with open("C:\\Users\\fhm\\source\\repos\\botRecord\\botRecord\\s3.json") as f:
+            data = json.load(f)
+        aws_access_key_id = data["ACCESS_KEY"]
+        aws_secret_access_key = data["SECRET_KEY"]
+        bucket = data["bucket"]
+        return aws_access_key_id, aws_secret_access_key, bucket
+
+    def __init__(self):
+        self.ACCESS_KEY, self.SECRET_KEY, self.bucket = self.readCredentials()
+        self.s3 = boto3.client(
+            "s3",
+            aws_access_key_id=self.ACCESS_KEY,
+            aws_secret_access_key=self.SECRET_KEY,
+        )
+
+    """
+        Upload a specific file to s3 bucket 
+    """
+
+    def AWSupload(self, local_file, s3_file):
+        try:
+            self.s3.upload_file(local_file, self.bucket, s3_file)
+            return True
+        except FileNotFoundError:
+            print("The file was not found")
+            return False
+        except NoCredentialsError:
+            print("Credentials not available")
+            return False
+
+    """
+        Download a specific file from s3 bucket
+    """
+
+    def AWSdownload(self, s3_fileName, localPath):
+        try:
+            # self.s3.download_file(self.bucket,s3_file, 'C:\\Users\\fhm\\Desktop\\dogs')
+            self.s3.download_file(self.bucket, s3_fileName, localPath)
+            return True
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "404":
+                print("The object does not exist.")
+            else:
+                raise
+
+    """
+        Returns a list with all the files in bucket (started by 'df')
+            prefixo: 'df' for example, all files starting by 'df'
+    """
+
+    def getFilesOnBucket(self, prefixo):
+        list = []
+        response = self.s3.list_objects(Bucket=self.bucket, Prefix=prefixo)
+        for elemento in response["Contents"]:
+            list.append(elemento["Key"])
+        return list
